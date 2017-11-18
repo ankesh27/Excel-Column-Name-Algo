7d273671b117d940354a10ca774d234eeec37a82
Ankesh <anki@Ankis-MacBook-Pro.local>
2017-11-18 23:21:45 +0530

    Stupid bug fixed

--- numberToLetters.php
+++ numberToLetters.php
@@ -6,10 +6,12 @@ Class NumberToLetters{
 	   $letter = chr(65 + $numeric);
 	   $num2 = intval(($num - 1) / 26);
 	   if ($num2 > 0) {
-	       return getNameFromNumber($num2) . $letter;
+	       return $this->getNameFromNumber($num2) . $letter;
 	   } else {
 	       return $letter;
 	   }
        }
 }
+$obj = new NumberToLetters();
+echo $obj->getNameFromNumber(1000000);
 ?>
